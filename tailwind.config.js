9520bed702d00bef5ba6ac4c7e940ee40b919a0c
mutiat <mutiat900@gmail.com>
2023-09-02 16:43:13 +0100

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tomato: "#ff6257",
+        orange: "#FF693A",
+        pink: "#FF537A",
+        grey: "#9294a0",
+        "darkslate-grey": "#242742",
+        "charcoal-grey": "#36384e",
+      },
+
+      fontFamily: {
+        rotobo: ["Roboto", "sans-serif"],
+      },
+
+      backgroundImage: {
+        "hero-mobile": "url('/illustration-sign-up-mobile.svg')",
+        "hero-desktop": "url('/illustration-sign-up-desktop.svg')",
+        "icon-list": "url('/icon-list.svg')",
+        "icon-success": "url('/icon-success.svg')",
+      },
+
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+
+      animation: {
+        fadeIn: "fadeIn 1s ease",
+      },
+    },
   },
   plugins: [],
 }
